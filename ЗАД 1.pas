d962ab125cadb16304ece70153e11a72874ae3bb
Ulyanka2901 <144227029+Ulyanka2901@users.noreply.github.com>
2023-12-08 13:21:26 +0300

    Add files via upload

---
+++ ЗАД 1.pas
@@ -0,0 +1,21 @@
+﻿program Zad1;
+const
+  N = 10;
+var
+  a:array[1..N] of integer;
+  i, max: integer;
+begin
+  writeln('Введите элементы массива (', N, ' шт.):');
+  for i:= 1 to N do
+  begin
+    readln(a[i]);
+  end;
+  max:= abs(a[1]);
+  for i:= 2 to N do
+  begin
+    if abs(a[i]) > max then
+      max:= abs(a[i]);
+  end;
+  writeln('Максимальный по модулю элемент:', max);
+  readln;
+end.
